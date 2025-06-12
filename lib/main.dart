5bed1543a416d756d9054cf4d8ce9938f04a480f
Angel Maldonado <151583275+Anng3l@users.noreply.github.com>
2025-06-12 16:00:11 -0500

    fc

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+           //Esto crea y PROVEE el estado a toda la app
+    return ChangeNotifierProvider(
+      //Crea un contexto para toda al app (para todos los widgets que se usan dentro de esta clase?)
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage()
+        
+      ),
+    );
+  }
+}
+                        
+//Define los datos que la app necesita para funcionar
+class MyAppState extends ChangeNotifier {
+                        //Clase que maneja el estado.
+                        //Esta clase en particular permite que si cambia el estado de los elementos de esta clase, se le sea notificado a otros widgets que un cambio sucedió.
+
+  //Por ahora sólo necesita esta variable
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea zzz:'),
+          Text(appState.current.asLowerCase),
+        
+
+
+          ElevatedButton(
+            onPressed: () {
+              print("Botón presionado");
+            },
+            child: Text("zzz"),
+          )
+
+
+
+
+
+
+
+          
+        ],
+      ),
+    );
+  }
+}
